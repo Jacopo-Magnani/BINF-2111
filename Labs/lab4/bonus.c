36ac11ad85abbe9b594990990e1753ae33e3a4fa
Jacopo-Magnani <114836149+Jacopo-Magnani@users.noreply.github.com>
2023-10-05 10:48:07 -0400

    Add files via upload

---
+++ Labs/lab4/bonus.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main() {
+
+	string arr[7] = {"Methionine", "Leucine", "Cysteine", "Alanine", "Valine", "Tyrosine", "Proline"};
+
+	for (int i = 0; i < 7; i++) {
+		printf("%s ", arr[i]);
+	}
+}
---
+++ Labs/lab4/bonus.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+arr=("Methionine""Leucine""Cysteine""Alanine""Valine""Tyrosine""Proline")
+
+for i in "${arr[@]}"
+do
+	echo "$i "
+done
