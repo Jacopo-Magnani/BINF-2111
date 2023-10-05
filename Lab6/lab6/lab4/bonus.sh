aaa8a9c4fcda0dedf7253c222223d6419d898393
Jacopo-Magnani <114836149+Jacopo-Magnani@users.noreply.github.com>
2023-10-05 10:46:26 -0400

    Delete Lab6/lab6 directory

--- Lab6/lab6/lab4/bonus.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-arr=("Methionine""Leucine""Cysteine""Alanine""Valine""Tyrosine""Proline")
-
-for i in "${arr[@]}"
-do
-	echo "$i "
-done
--- Lab6/lab6/lab4/question5.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-arr=("Methionine""Leucine""Cysteine""Alanine""Valine""Tyrosine""Proline")
-unset 'arr[3]'
-echo  ${arr[@]:2:4}
-array+=('Histidine')
-
